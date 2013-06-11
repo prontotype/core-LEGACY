e7c7ee22c2108d690e7bf593f722c1dc837218a2
Mark Perkins <mark@allmarkedup.com>
2013-06-11 11:12:14 +0100

    Add in assets snippet handler with stylesheet tag

---
+++ src/Prontotype/Snippets/Assets.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Prontotype\Snippets;
+
+Class Assets extends Base {
+    
+    protected $templatePath = 'system/snippets/assets';
+    
+    protected $configKey = 'assets';
+    
+    public function stylesheet($href, $media = 'all', $appendAssetPath = true)
+    {
+        if ($appendAssetPath) {
+            $href = $this->prefixPath($href);
+        }
+        
+        $attrs = $this->mergeOpts(array(
+            'type'  => 'text/css',
+            'href'  => $href,
+            'media' => $media,
+        ), $attrs);
+        
+        return $this->renderTemplate('stylesheet.twig', array(
+            'attrs'  => $attrs,
+        ));
+    }
+    
+    public function prefixPath($path)
+    {        
+        $path = '/' . $this->app['pt.config']['triggers']['assets'] . '/' . trim($path, '/');
+        if ( ! $this->app['pt.config']['clean_urls'] ) {
+            $path = '/index.php' . $path;
+        }
+        return $path;
+    }
+    
+}
