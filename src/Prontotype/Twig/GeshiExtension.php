47d205c5d7a08881c8cda8ee999df1a012795514
Mark Perkins <mark@allmarkedup.com>
2013-06-27 10:42:23 +0100

    Use Geshi for highlighting code blocks in docs

--- src/Prontotype/Twig/GeshiExtension.php
+++ src/Prontotype/Twig/GeshiExtension.php
@@ -29,11 +29,11 @@ class GeshiExtension extends \Twig_Extension
     public function replaceGeshiPre($string)
     {
         $pattern = '#<pre class="brush: (.+?)">(.+?)</pre>#si'; 
- 
         $output = preg_replace_callback($pattern, function($matches) {
-            return $this->geshiHighlight($matches[2], $mathes[1]); 
-        }, $string); 
- 
+            $geshi = new \GeSHi($matches[2], $matches[1]);
+            $geshi->enable_classes();
+            return $geshi->parse_code();
+        }, $string);
         return $output; 
     }
  
