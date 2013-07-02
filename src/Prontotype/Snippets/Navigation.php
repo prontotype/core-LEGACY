d89e38823afc96168aedcf2e218f24e16738f3bd
Mark Perkins <mark@allmarkedup.com>
2013-07-02 21:05:58 +0100

    Add basic breadcrumb snippet

--- src/Prontotype/Snippets/Navigation.php
+++ src/Prontotype/Snippets/Navigation.php
@@ -24,7 +24,7 @@ Class Navigation extends Base {
         
         if ( $startPage === null ) {
             // get the homepage
-            $startPage = $this->app['pt.pages']->getByUrlPath('/' . $this->app['pt.prototype.path']);
+            $startPage = $this->app['pt.pages']->getByUrlPath('/');
         } elseif ( is_string($startPage) ) {
             // start page is an ID?
             if ( ! $startPage = $this->app['pt.pages']->getById($startPage) ) {
@@ -44,7 +44,42 @@ Class Navigation extends Base {
         return $this->renderTemplate('page-tree.twig', array(
             'pages' => $pages,
             'level' => $level,
-            'opts' => $opts,
+            'opts'  => $opts,
+            'attrs' => $attrs
+        ));
+    }
+    
+    public function breadcrumb($opts = array(), $attrs = array())
+    {
+        if ( ! $opts ) {
+            $opts = array();
+        }
+        
+        $opts = $this->mergeOpts(array(
+            'type' => 'ul',
+        ), $opts);
+        
+        $pages = array(
+            $this->app['pt.pages']->getByUrlPath('/')
+        );
+        
+        $urlPath = $this->app['pt.request']->getUrlPath();
+        if ( !empty($this->app['pt.prototype.path']) ) {
+            $urlPath = preg_replace("/^(\\" . $this->app['pt.prototype.path'] . ")/", '', $urlPath);            
+        }
+
+        $urlParts = explode('/', trim($urlPath,'/') );
+        $builtPath = '/' . $this->app['pt.prototype.path'];
+        if ( count($urlParts) ) {
+            foreach( $urlParts as $urlPart ) {
+                $builtPath .= '/' . $urlPart;
+                $pages[] = $this->app['pt.pages']->getByUrlPath($builtPath);
+            }
+        }
+        
+        return $this->renderTemplate('breadcrumb.twig', array(
+            'pages' => $pages,
+            'opts'  => $opts,
             'attrs' => $attrs
         ));
     }
